74dca08a5ce1d4430d1ef65626514a87c3fd7e42
pratypr1 <pratypr1@in.ibm>
2019-02-12 18:26:01 +0530

    config modified

--- src/main/java/com/stackroute/springbootkafkaproducer/config/KafkaConfiguration.java
+++ src/main/java/com/stackroute/springbootkafkaproducer/config/KafkaConfiguration.java
@@ -16,7 +16,7 @@ import java.util.Map;
 @Configuration
 public class KafkaConfiguration {
     @Bean
-    public ProducerFactory<String , User> producerFactory()
+    public ProducerFactory<String, User> producerFactory()
     {
 
         Map<String, Object> config=new HashMap<>();
